b5d848fdaacf92883a64507318fd0750144e6f60
Justus <justus.d2025@gmail.com>
2025-06-18 14:05:34 +0200

    fixed bug

--- goldbach.py
+++ goldbach.py
@@ -2,17 +2,6 @@ def is_prime(number:int) -> bool:
     """ Checks if a given integer is a prime number using a list comprehension. """
     return [(number-1)%n for n in range(1,number)].count(True) == 1
 
-def short_sotp(number):
-    """
-    My own list comprehension solution. Does the same as sum_of_two_primes!
-    """
-    already_used: list = []
-    for result in [[[i,j] for j in range(number - i,number) if is_prime(j) and i + j == number] for i in range(2,number) if not number%2 and is_prime(i) and number - i >= 2]:
-        if result:
-            result[0].sort()
-            if result[0] not in already_used:
-                already_used.append(result[0])
-    return already_used
 
 def sum_of_two_primes(number):
     """ Attempts to find pairs of prime numbers that sum up to a given even number. """
@@ -40,7 +29,7 @@ def check_user_input_until_integer(prompt:str) -> str:
 def main():
     """ This is where the action goes """
     number = int(check_user_input_until_integer("Enter a number: "))
-    for a,b in short_sotp(number):
+    for a,b in sum_of_two_primes(number):
         print(f"The number {number} equals to the sum of {a} and {b}")
 
 if __name__ == "__main__":
