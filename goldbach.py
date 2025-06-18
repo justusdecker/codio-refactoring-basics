c091f024c7b8c3ccfb60f93a069b3d2a8b24baf3
Justus <justus.d2025@gmail.com>
2025-06-18 14:04:07 +0200

    fixed issue: missing docstrings

--- goldbach.py
+++ goldbach.py
@@ -1,5 +1,7 @@
 def is_prime(number:int) -> bool:
+    """ Checks if a given integer is a prime number using a list comprehension. """
     return [(number-1)%n for n in range(1,number)].count(True) == 1
+
 def short_sotp(number):
     """
     My own list comprehension solution. Does the same as sum_of_two_primes!
@@ -13,6 +15,7 @@ def short_sotp(number):
     return already_used
 
 def sum_of_two_primes(number):
+    """ Attempts to find pairs of prime numbers that sum up to a given even number. """
     if number%2: return []
     already_used: list = []
     for i in range(2,number):
@@ -27,12 +30,18 @@ def sum_of_two_primes(number):
     return already_used
 
 def check_user_input_until_integer(prompt:str) -> str:
+    """ Continuously prompts the user for input until a string representing an integer is provided. """
     user_input: str = ""
     while not user_input:
         user_input = input(prompt)
         user_input = user_input if user_input.isdecimal() else ""
     return user_input
-if __name__ == "__main__":
+
+def main():
+    """ This is where the action goes """
     number = int(check_user_input_until_integer("Enter a number: "))
     for a,b in short_sotp(number):
         print(f"The number {number} equals to the sum of {a} and {b}")
+
+if __name__ == "__main__":
+    main()
