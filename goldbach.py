925c6f83018d988bec54366deab2adafd5911c59
Justus <justus.d2025@gmail.com>
2025-06-18 14:10:11 +0200

    b

--- goldbach.py
+++ goldbach.py
@@ -16,17 +16,20 @@ def sum_of_two_primes(number):
                 already_used.append(result)
     return already_used
 
-def check_user_input_until_integer(prompt:str) -> str:
-    """ Continuously prompts the user for input until a string representing an integer is provided. """
+def check_user_input_until_integer(prompt:str) -> int:
+    """
+    Continuously prompts the user for input until a valid integer is provided.
+    Returns the to int converted user_input
+    """
     user_input: str = ""
     while not user_input:
         user_input = input(prompt)
         user_input = user_input if user_input.isdecimal() else ""
-    return user_input
+    return int(user_input)
 
 def main():
     """ This is where the action goes """
-    number = int(check_user_input_until_integer("Enter a number: "))
+    number = check_user_input_until_integer("Enter a number: ")
     for a,b in sum_of_two_primes(number):
         print(f"The number {number} equals to the sum of {a} and {b}")
 
